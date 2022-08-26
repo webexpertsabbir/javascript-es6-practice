159a7ca5df311cebe9d306f2714cd7194ce30f8f
Sabbir <webexpertsabbir@gmail.com>
2022-08-26 11:24:45 +0600

    add q2

---
+++ questionTamplate.js
@@ -0,0 +1,12 @@
+// ২. টেম্পলেট স্ট্রিং দিয়ে একটা স্ট্রিং তৈরি করো। সেটার মধ্যে উপরে ডিক্লেয়ার করা ভেরিয়েবল এর মান ডাইনামিক ভাবে বসাও। একইভাবে উপরে একটা অবজেক্ট ডিক্লেয়ার করো। এবং ডাইনামিকভাবে উপরের অবজেক্ট এর দুইটা প্রপার্টি এর মান তোমার টেমপ্লেট স্ট্রিং এর মধ্যে বসাও। 
+
+
+const student = {
+    name: 'Sabbir Hossen',
+    class: '13',
+    university: 'Dafodil'
+}
+
+const studentDitels = `${student.name} is good student,
+ university of ${student.university}`;
+console.log(studentDitels);
